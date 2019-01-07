b9b7ceaffd3b3af230d50cacd2815c8881d43e23
Stephen Argentina <churchman1993@yahoo.com>
2019-01-07 17:49:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here:
+function driversWithRevenueOver(array, number) {
+    return array.filter(driver => driver.revenue > number)
+}
+
+function driverNamesWithRevenueOver(array, number) {
+    return array.filter(driver => driver.revenue > number).map(driver => driver.name)
+}
+
+function exactMatch(array, o){
+    return array.filter(driver => driver[Object.keys(o)[0]] == o[Object.keys(o)[0]])
+}
+
+function exactMatchToList(array, o){
+    return array.filter(driver => driver[Object.keys(o)[0]] == o[Object.keys(o)[0]]).map(driver => driver.name)
+}
+const drivers = [
+    { name: 'Sally',   revenue: 400 },
+    { name: 'Sally',   revenue: 200 },
+    { name: 'Annette', revenue: 200 }
+  ]
+
+// console.log(driversWithRevenueOver(array, 300))
+console.log(exactMatchToList(drivers, {name: "Sally"}))
+
